2e0827f6ea8d97da6023f3a95b229842afdc7eb2
Piyush Dalmia <invictusgamer9900@gmail.com>
2024-09-10 13:10:07 +0530

    signup,login,contact storing data in state done and page not found 404 page added again

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyD1HVWIoMM0-9njylHpfhI18YSCmG9d-uI",
-  authDomain: "jabwemeet-51e30.firebaseapp.com",
-  databaseURL: "https://jabwemeet-51e30-default-rtdb.firebaseio.com",
-  projectId: "jabwemeet-51e30",
-  storageBucket: "jabwemeet-51e30.appspot.com",
-  messagingSenderId: "350958865229",
-  appId: "1:350958865229:web:143a4952447a4a27f38504",
-  measurementId: "G-19ERR5G91S"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
